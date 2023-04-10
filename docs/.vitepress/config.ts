e9cb80f0337708da07f90c3db50ede9e52c7c3c1
prodigytech <121081350+Metaworld2023@users.noreply.github.com>
2023-04-10 20:22:39 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -30,7 +30,7 @@ export default defineConfigWithTheme<DefaultTheme.Config>({
       },
       {
         text: '安装使用',
-        link: '/md/1.1.md',
+        link: '/md/1.md',
         collapsible: true,
         collapsed: true,
         items: [
@@ -45,7 +45,7 @@ export default defineConfigWithTheme<DefaultTheme.Config>({
       },
       {
         text: '脚本通信',
-        link: '/md/2.1.md',
+        link: '/md/2.md',
         collapsible: true,
         collapsed: true,
         items: [
@@ -59,7 +59,7 @@ export default defineConfigWithTheme<DefaultTheme.Config>({
       },
       {
         text: '基础入门',
-        link: '/md/3.1.md',
+        link: '/md/3.md',
         collapsible: true,
         collapsed: true,
         items: [
@@ -69,44 +69,55 @@ export default defineConfigWithTheme<DefaultTheme.Config>({
           { text: '初生点', link: '/md/3.4.md' },
           { text: '角色动画', link: '/md/3.5.md' },
           { text: '游戏物体的使用', link: '/md/3.6.md' },
+          { text: '游戏物体父子级', link: '/md/3.7.md' },
           { text: '玩家与角色', link: '/md/3.8.md' },
           { text: '角色编辑器', link: '/md/3.9.md' },
           { text: '摄像机', link: '/md/3.0.1.md' },
-          { text: '数据持久化', link: '/md/3.0.2.md' }
+          { text: '数据持久化', link: '/md/3.0.2.md' },
+          { text: '冲量', link: '/md/3.0.3.md' },
+          { text: '交互物', link: '/md/3.0.4.md' },
+          { text: '射线检测', link: '/md/3.0.5.md' },
+          { text: '投掷物', link: '/md/3.0.6.md' }   
         ]
       },
       {
         text: '跳一跳小游戏',
-        link: '/md/4.1.md',
+        link: '/md/4.md',
         collapsible: true,
         collapsed: true,
         items: [
           { text: '创建工程', link: '/md/4.1.md' },
           { text: '播放游戏音乐', link: '/md/4.2.md' },
           { text: '添加角色特效', link: '/md/4.3.md' },
-          { text: '搭建跳跃场景', link: '/md/4.4.md' }
+          { text: '搭建跳跃场景', link: '/md/4.4.md' },
+          { text: '添加失败区域', link: '/md/4.5.md' },
+          { text: '添加胜利区域', link: '/md/4.6.md' }
         ]
       },
       {
         text: '游戏界面',
-        link: '/md/5.1.md',
+        link: '/md/5.md',
         collapsible: true,
         collapsed: true,
         items: [
           { text: '创建UI', link: '/md/5.1.md' },
           { text: '图片', link: '/md/5.2.md' },
-          { text: '5.0.1', link: '/md/5.0.1.md' },
-          { text: '5.0.2', link: '/md/5.0.2.md' },
-          { text: '5.0.3', link: '/md/5.0.3.md' },
-          { text: '5.0.4', link: '/md/5.0.4.md' },
-          { text: '5.0.5', link: '/md/5.0.5.md' },
-          { text: '5.0.6', link: '/md/5.0.6.md' },
-          { text: '5.0.7', link: '/md/5.0.7.md' }
+          { text: '文本', link: '/md/5.3.md' },
+          { text: '按钮', link: '/md/5.4.md' },
+          { text: '容器', link: '/md/5.5.md' },
+          { text: '快速布局', link: '/md/5.0.1.md' },
+          { text: 'UI自适应对齐', link: '/md/5.0.2.md' },
+          { text: 'UI显示/隐藏', link: '/md/5.0.3.md' },
+          { text: '输入框', link: '/md/5.0.4.md' },
+          { text: '进度条', link: '/md/5.0.5.md' },
+          { text: '滚动框', link: '/md/5.0.6.md' },
+          { text: '加载图', link: '/md/5.0.7.md' },
+          { text: '遥感', link: '/md/5.0.8.md' }
         ]
       },
       {
         text: '常用代码片段',
-        link: '/md/6.2.1.md',
+        link: '/md/6.2.md',
         collapsible: true,
         collapsed: true,
         items: [
@@ -121,14 +132,17 @@ export default defineConfigWithTheme<DefaultTheme.Config>({
       },
       {
         text: '炸弹人小游戏',
-        link: '/md/7.1.md',
+        link: '/md/7.md',
         collapsible: true,
         collapsed: true,
         items: [
           { text: '创建工程', link: '/md/7.1.md' },
           { text: '入口类与控制界面', link: '/md/7.2.md' },
           { text: '炸弹预制体', link: '/md/7.3.md' },
-          { text: '添加游戏场景', link: '/md/7.6.md' }
+          { text: '主角释放炸弹', link: '/md/7.4.md' },
+          { text: '血量UI与数据', link: '/md/7.5.md' },
+          { text: '添加游戏场景', link: '/md/7.6.md' },
+          { text: '制作一个减速区域', link: '/md/7.7.md' }
         ]
       }
     ],
