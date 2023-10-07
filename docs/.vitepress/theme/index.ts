5bbcbb4ddec44d18632b0762e45594b6f32c8286
yuechao.xu <yuechao.xu@appshahe.com>
2023-10-07 09:56:32 +0800

    feat: 升级主题版本

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,24 +1,7 @@
 import VPTheme from 'metaapp-prodigytech-doc-theme'
-import LayoutIndex from './Index.vue'
-import codeblocksFold from 'vitepress-plugin-codeblocks-fold' // 导入方法
-import 'vitepress-plugin-codeblocks-fold/style/index.scss' // 导入样式
 import { useData, useRoute } from 'vitepress'
 import './style.css'
 
-import 'gitalk/dist/gitalk.css'
-
 export default {
-  ...VPTheme,
-  enhanceApp(ctx) {
-    VPTheme.enhanceApp(ctx)
-    // ...
-  },
-  Layout: LayoutIndex,
-  setup() {
-    // 获取前言和路由
-    const { frontmatter } = useData()
-    const route = useRoute()
-    // 基础使用
-    codeblocksFold({ route, frontmatter }, true, 400)
-  }
+  ...VPTheme
 }
